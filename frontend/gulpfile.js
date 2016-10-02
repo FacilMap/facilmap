5863ddac5907813bed8c499dc77418b4b8b626fa
Candid Dauth <cdauth@cdauth.eu>
2016-10-02 20:23:29 +0300

    Build source maps

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -7,13 +7,14 @@ var clean = require("gulp-clean");
 var concat = require("gulp-concat");
 var cssBase64 = require("gulp-css-base64");
 var gulpIf = require("gulp-if");
-var minifyCss = require("gulp-minify-css");
+var cleanCss = require("gulp-clean-css");
 var newer = require("gulp-newer");
 var ngAnnotate = require("gulp-ng-annotate");
 var replace = require("gulp-replace");
 var uglify = require("gulp-uglify");
 var mainBowerFiles = require("main-bower-files");
 var combine = require("stream-combiner");
+var sourcemaps = require("gulp-sourcemaps");
 
 var files = [
 	"js/*.js",
@@ -41,13 +42,17 @@ gulp.task("deps", function() {
 		gulpIf([ "**/*.js", "**/*.css" ], combine(
 			gulpIf("**/*.js", combine(
 				newer("build/dependencies.js"),
+				sourcemaps.init(),
 				concat("dependencies.js"),
-				uglify()
+				uglify(),
+				sourcemaps.write("./sourcemaps")
 			)),
 			gulpIf("**/*.css", combine(
 				newer("build/dependencies.css"),
+				sourcemaps.init(),
 				concat("dependencies.css"),
-				minifyCss()
+				cleanCss(),
+				sourcemaps.write("./sourcemaps")
 			)),
 			gulp.dest("build")
 		))
@@ -60,15 +65,19 @@ gulp.task("app", function() {
 		gulpIf("**/*.html", templateCache({ module: "facilpad", base: process.cwd() + "/templates/" })),
 		gulpIf("**/*.js", combine(
 			newer("build/app.js"),
+			sourcemaps.init(),
 			concat("app.js"),
 			ngAnnotate(),
-			uglify()
+			uglify(),
+			sourcemaps.write("./sourcemaps")
 		)),
 		gulpIf("**/*.css", combine(
 			newer("build/app.css"),
 			cssBase64({ maxWeightResource: 1000000 }),
+			sourcemaps.init(),
 			concat("app.css"),
-			minifyCss()
+			cleanCss(),
+			sourcemaps.write("./sourcemaps")
 		)),
 		gulp.dest("build")
 	);
@@ -81,11 +90,15 @@ gulp.task("all", [ "deps", "app" ], function(callback) {
 				es.merge(
 					combine(
 						gulp.src([ "build/dependencies.js", "build/app.js" ]),
-						concat("all.js")
+						sourcemaps.init({ loadMaps: true }),
+						concat("all.js"),
+						sourcemaps.write("./sourcemaps")
 					),
 					combine(
 						gulp.src([ "build/dependencies.css", "build/app.css" ]),
-						concat("all.css")
+						sourcemaps.init({ loadMaps: true }),
+						concat("all.css"),
+						sourcemaps.write("./sourcemaps")
 					)
 				),
 				gulp.dest("build"),
