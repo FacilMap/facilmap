a2c395e0a23aa0a1c3db95062d63ac4e048c3551
Candid Dauth <cdauth@cdauth.eu>
2016-10-04 14:21:20 +0300

    Migrate "Save View" and "Manage Views" dialogs to bootstrap

--- frontend/app/map/views/views.js
+++ frontend/app/map/views/views.js
@@ -1,60 +1,77 @@
 (function(fp, $, ng, undefined) {
 
-	fp.app.factory("fpMapViews", function(fpDialogs) {
+	fp.app.factory("fpMapViews", function($uibModal) {
 		return function(map) {
 			var ret = {
 				saveView : function() {
-					var scope = map.socket.$new();
-					scope.name = null;
-
-					scope.save = function(makeDefault) {
-						var view = map.getCurrentView();
-						view.name = scope.name;
-						map.socket.emit("addView", view, function(err, view) {
-							if(err)
-								return scope.error = err;
-
-							if(makeDefault) {
-								map.socket.emit("editPad", { defaultViewId: view.id }, function(err) {
-									if(err)
-										return scope.error = err;
-
-									scope.dialog.close();
-								});
-							}
-							else
-								scope.dialog.close();
-						});
-					};
-
-					scope.dialog = fpDialogs.open("map/views/save-view.html", scope, "Save current view");
+					$uibModal.open({
+						templateUrl: "map/views/save-view.html",
+						scope: map.socket,
+						controller: "fpMapViewsSaveCtrl",
+						size: "lg",
+						resolve: {
+							map: function() { return map; }
+						}
+					});
 				},
 				manageViews : function() {
-					var scope = map.socket.$new();
-
-					scope.display = function(view) {
-
-					};
-
-					scope.makeDefault = function(view) {
-						map.socket.emit("editPad", { defaultViewId: view.id }, function(err) {
-							if(err)
-								scope.error = err;
-						});
-					};
-
-					scope['delete'] = function(view) {
-						map.socket.emit("deleteView", { id: view.id }, function(err) {
-							if(err)
-								scope.error = err;
-						});
-					};
-
-					scope.dialog = fpDialogs.open("map/views/manage-views.html", scope, "Manage views");
+					$uibModal.open({
+						templateUrl: "map/views/manage-views.html",
+						scope: map.socket,
+						controller: "fpMapViewsManageCtrl",
+						size: "lg",
+						resolve: {
+							map: function() { return map; }
+						}
+					});
 				}
 			};
 
 			return ret;
+		};
+	});
+
+	fp.app.controller("fpMapViewsSaveCtrl", function($scope, map) {
+		$scope.name = null;
+
+		$scope.save = function(makeDefault) {
+			var view = map.getCurrentView();
+			view.name = $scope.name;
+			map.socket.emit("addView", view, function(err, view) {
+				if(err)
+					return $scope.error = err;
+
+				if(makeDefault) {
+					map.socket.emit("editPad", { defaultViewId: view.id }, function(err) {
+						if(err)
+							return $scope.error = err;
+
+						$scope.$close();
+					});
+				}
+				else
+					$scope.$close();
+			});
+		};
+	});
+
+	fp.app.controller("fpMapViewsManageCtrl", function($scope, map) {
+		$scope.display = function(view) {
+
+		};
+
+		$scope.makeDefault = function(view) {
+			map.socket.emit("editPad", { defaultViewId: view.id }, function(err) {
+				if(err)
+					$scope.error = err;
+			});
+		};
+
+		$scope['delete'] = function(view) {
+			map.socket.emit("deleteView", { id: view.id }, function(err) {
+				if(err)
+					$scope.error = err;
+			});
 		};
 	});
 
