92fb1bf1af9142c2c711453fcb00453676c10543
Candid Dauth <cdauth@cdauth.eu>
2017-02-10 15:36:38 +0100

    Load icons from github instead of bower

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,6 +3,10 @@ var gutil = require("gulp-util");
 var clean = require("gulp-clean");
 var newer = require("gulp-newer");
 var combine = require("stream-combiner");
+var fs = require("fs");
+var Promise = require("promise");
+var request = require("request-promise");
+var unzip = require("unzipper");
 var webpack = require("webpack");
 
 var icons = require("./gulpfile-icons");
@@ -19,9 +23,34 @@ gulp.task("clean", function() {
 	);
 });
 
-gulp.task("icons", function() {
+gulp.task("download-icons", function() {
+	return new Promise((resolve, reject) => {
+		fs.exists("build/Open-SVG-Map-Icons", (exists) => {
+			resolve(exists);
+		});
+	}).then((exists) => {
+		if(exists)
+			return;
+
+		let extract = unzip.Extract({
+			path: "build/"
+		});
+
+		let download = request.get("https://github.com/twain47/Open-SVG-Map-Icons/archive/master.zip");
+		download.pipe(extract);
+		download.catch((err) => {
+			extract.emit("error", err);
+		});
+
+		return extract.promise().then(() => {
+			return Promise.denodeify(fs.rename)("build/Open-SVG-Map-Icons-master", "build/Open-SVG-Map-Icons");
+		});
+	});
+});
+
+gulp.task("icons", ["download-icons"], function() {
 	return combine(
-		gulp.src("../bower_components/Open-SVG-Map-Icons/svg/**/*.svg"),
+		gulp.src("build/Open-SVG-Map-Icons/svg/**/*.svg"),
 		newer("build/icons.js"),
 		icons("icons.js", "angular.module(\"facilmap\").constant(\"fmIcons\", %s);"),
 		gulp.dest("build")
