9c20ef6a0d653f2076ce1b13fecb057ee87cd361
Candid Dauth <cdauth@cdauth.eu>
2021-04-14 02:39:03 +0200

    Make multiple result info scrollable

--- frontend/src/map/multiple-info/multiple-info.vue
+++ frontend/src/map/multiple-info/multiple-info.vue
@@ -1,15 +1,19 @@
 <div class="fm-multiple-info">
 	<b-carousel :interval="0" v-model="activeTab">
 		<b-carousel-slide>
-			<b-list-group-item v-for="object in objects" active>
-				<span>
-					<a href="javascript:" @click="$emit('click-object', object, $event)">{{object.name}}</a>
-					{{" "}}
-					<span class="result-type" v-if="client.types[object.typeId]">({{client.types[object.typeId].name}})</span>
-				</span>
-				<a href="javascript:" @click="zoomToObject(object)" v-b-tooltip.hover.left="'Zoom to object'"><Icon icon="zoom-in" alt="Zoom"></Icon></a>
-				<a href="javascript:" @click="openObject(object)" v-b-tooltip.hover.right="'Show details'"><Icon icon="arrow-right" alt="Details"></Icon></a>
-			</b-list-group-item>
+			<div class="fm-search-box-collapse-point">
+				<b-list-group>
+					<b-list-group-item v-for="object in objects" active>
+						<span>
+							<a href="javascript:" @click="$emit('click-object', object, $event)">{{object.name}}</a>
+							{{" "}}
+							<span class="result-type" v-if="client.types[object.typeId]">({{client.types[object.typeId].name}})</span>
+						</span>
+						<a href="javascript:" @click="zoomToObject(object)" v-b-tooltip.hover.left="'Zoom to object'"><Icon icon="zoom-in" alt="Zoom"></Icon></a>
+						<a href="javascript:" @click="openObject(object)" v-b-tooltip.hover.right="'Show details'"><Icon icon="arrow-right" alt="Details"></Icon></a>
+					</b-list-group-item>
+				</b-list-group>
+			</div>
 
 			<b-button-toolbar>
 				<b-button v-b-tooltip.hover="'Zoom to selection'" @click="zoom()" size="sm"><Icon icon="zoom-in" alt="Zoom to selection"></Icon></b-button>
