78199d93512f6ac8624a039549e83f5880c190cf
Candid Dauth <cdauth@cdauth.eu>
2023-09-24 23:00:38 +0200

    WIP

---
+++ utils/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import dtsPlugin from 'vite-plugin-dts';
+import autoExternalPlugin from 'rollup-plugin-auto-external';
+
+export default defineConfig({
+	plugins: [
+		dtsPlugin({ rollupTypes: true }),
+		autoExternalPlugin()
+	],
+	build: {
+		sourcemap: true,
+		minify: false,
+		target: "esnext",
+		lib: {
+			entry: './src/index.ts',
+			name: 'facilmap-utils',
+			fileName: () => 'facilmap-utils.mjs',
+			formats: ['es']
+		}
+	}
+});
