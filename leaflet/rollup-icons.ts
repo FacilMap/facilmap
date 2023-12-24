558067bb69ace40f8c5fbadd756e22746d184e5d
Candid Dauth <cdauth@cdauth.eu>
2023-12-24 18:39:43 +0100

    Fix icons

--- leaflet/rollup-icons.ts
+++ leaflet/rollup-icons.ts
@@ -1,7 +1,10 @@
 import type { Plugin } from "rollup";
+// eslint-disable-next-line import/no-named-as-default
 import glob from "fast-glob";
 import { readFile } from "fs/promises";
 import { createRequire } from "module";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 // https://stackoverflow.com/a/62499498/242365
 const require = createRequire(import.meta.url);
@@ -17,7 +20,7 @@ export default function iconPlugin(): Plugin {
 		load: async (id) => {
 			if (id === 'virtual:icons') {
 				const icons: Record<string, Record<string, string>> = {};
-				for (const path of await glob('./assets/icons/*/*.svg')) {
+				for (const path of await glob(`${dirname(fileURLToPath(import.meta.url))}/assets/icons/*/*.svg`)) {
 					const [set, fname] = path.split("/").slice(-2);
 
 					if (!icons[set])
