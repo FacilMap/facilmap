485aa98e2b757c57391c9943b307548ffc1fe2a0
Candid Dauth <cdauth@cdauth.eu>
2024-02-17 04:31:46 +0100

    Migrate to Express 5

--- server/src/webserver.ts
+++ server/src/webserver.ts
@@ -1,5 +1,5 @@
 import compression from "compression";
-import express, { type Request, type Response, type NextFunction } from "express";
+import express, { type Request, type Response } from "express";
 import { createServer, type Server as HttpServer } from "http";
 import type { PadId } from "facilmap-types";
 import { createTable } from "./export/table.js";
@@ -16,43 +16,39 @@ type PathParams = {
 }
 
 export async function initWebserver(database: Database, port: number, host?: string): Promise<HttpServer> {
-	const padMiddleware = async function(req: Request<PathParams>, res: Response<string>, next: NextFunction) {
-		try {
-			let params: RenderMapParams;
-			if(req.params?.padId) {
-				const padData = await database.pads.getPadDataByAnyId(req.params.padId);
-				if (padData) {
-					params = {
-						padData: {
-							searchEngines: padData.searchEngines,
-							name: normalizePadName(padData.name),
-							description: padData.description
-						},
-						isReadOnly: padData.id === req.params.padId
-					};
-				} else {
-					res.status(404);
-					params = {
-						padData: {
-							searchEngines: false,
-							name: "",
-							description: ""
-						},
-						isReadOnly: true
-					};
-				}
-			} else {
+	const padMiddleware = async (req: Request<PathParams>, res: Response<string>) => {
+		let params: RenderMapParams;
+		if(req.params?.padId) {
+			const padData = await database.pads.getPadDataByAnyId(req.params.padId);
+			if (padData) {
 				params = {
-					padData: undefined,
+					padData: {
+						searchEngines: padData.searchEngines,
+						name: normalizePadName(padData.name),
+						description: padData.description
+					},
+					isReadOnly: padData.id === req.params.padId
+				};
+			} else {
+				res.status(404);
+				params = {
+					padData: {
+						searchEngines: false,
+						name: "",
+						description: ""
+					},
 					isReadOnly: true
 				};
 			}
-
-			res.type("html");
-			res.send(await renderMap(params));
-		} catch (err: any) {
-			next(err);
+		} else {
+			params = {
+				padData: undefined,
+				isReadOnly: true
+			};
 		}
+
+		res.type("html");
+		res.send(await renderMap(params));
 	};
 
 	const app = express();
@@ -66,50 +62,38 @@ export async function initWebserver(database: Database, port: number, host?: str
 	// If no file with this name has been found, we render a pad
 	app.get("/:padId", padMiddleware);
 
-	app.get("/:padId/gpx", async function(req: Request<PathParams>, res: Response<string>, next) {
-		try {
-			const padData = await database.pads.getPadDataByAnyId(req.params.padId);
+	app.get("/:padId/gpx", async (req: Request<PathParams>, res: Response<string>) => {
+		const padData = await database.pads.getPadDataByAnyId(req.params.padId);
 
-			if(!padData)
-				throw new Error(`Map with ID ${req.params.padId} could not be found.`);
+		if(!padData)
+			throw new Error(`Map with ID ${req.params.padId} could not be found.`);
 
-			res.set("Content-type", "application/gpx+xml");
-			res.attachment(padData.name.replace(/[\\/:*?"<>|]+/g, '_') + ".gpx");
-			exportGpx(database, padData ? padData.id : req.params.padId, req.query.useTracks == "1", req.query.filter as string | undefined).pipeTo(Writable.toWeb(res));
-		} catch (e) {
-			next(e);
-		}
+		res.set("Content-type", "application/gpx+xml");
+		res.attachment(padData.name.replace(/[\\/:*?"<>|]+/g, '_') + ".gpx");
+		exportGpx(database, padData ? padData.id : req.params.padId, req.query.useTracks == "1", req.query.filter as string | undefined).pipeTo(Writable.toWeb(res));
 	});
 
-	app.get("/:padId/table", async function(req: Request<PathParams>, res: Response<string>, next) {
-		try {
-			res.type("html");
-			res.send(await createTable(
-				database,
-				req.params.padId,
-				req.query.filter as string | undefined,
-				req.query.hide ? (req.query.hide as string).split(',') : []
-			));
-		} catch (e) {
-			next(e);
-		}
+	app.get("/:padId/table", async (req: Request<PathParams>, res: Response<string>) => {
+		res.type("html");
+		res.send(await createTable(
+			database,
+			req.params.padId,
+			req.query.filter as string | undefined,
+			req.query.hide ? (req.query.hide as string).split(',') : []
+		));
 	});
 
-	app.get("/:padId/geojson", async function(req: Request<PathParams>, res: Response<string>, next) {
-		try {
-			const padData = await database.pads.getPadData(req.params.padId);
+	app.get("/:padId/geojson", async (req: Request<PathParams>, res: Response<string>) => {
+		const padData = await database.pads.getPadData(req.params.padId);
 
-			if(!padData)
-				throw new Error(`Map with ID ${req.params.padId} could not be found.`);
+		if(!padData)
+			throw new Error(`Map with ID ${req.params.padId} could not be found.`);
 
-			res.set("Content-type", "application/geo+json");
-			res.attachment(padData.name.replace(/[\\/:*?"<>|]+/g, '_') + ".geojson");
+		res.set("Content-type", "application/geo+json");
+		res.attachment(padData.name.replace(/[\\/:*?"<>|]+/g, '_') + ".geojson");
 
-			const result = exportGeoJson(database, req.params.padId, req.query.filter as string | undefined);
-			Readable.fromWeb(result).pipe(res);
-		} catch (e) {
-			next(e);
-		}
+		const result = exportGeoJson(database, req.params.padId, req.query.filter as string | undefined);
+		Readable.fromWeb(result).pipe(res);
 	});
 
 	const server = createServer(app);
