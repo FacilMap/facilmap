2999e4646cb6f1a0b449ea4e40691ffec6674ce5
Candid Dauth <cdauth@cdauth.eu>
2024-03-09 05:25:26 +0100

    Apply workaround for failing test

--- utils/vite.config.ts
+++ utils/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import dtsPlugin from "vite-plugin-dts";
 
@@ -17,6 +18,13 @@ export default defineConfig({
 		},
 		rollupOptions: {
 			external: (id) => id.includes("/node_modules/") || (!id.startsWith("./") && !id.startsWith("../") && /* resolved internal modules */ !id.startsWith("/"))
+		}
+	},
+	test: {
+		server: {
+			deps: {
+				external: [/\/node_modules\//] // Temporary fix for https://discord.com/channels/917386801235247114/1215127757550260225
+			}
 		}
 	}
 });
