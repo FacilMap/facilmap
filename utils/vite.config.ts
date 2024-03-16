3d3110607c6abd378ad34c58d3f181f1259f3078
Candid Dauth <cdauth@cdauth.eu>
2024-03-16 01:02:32 +0100

    Fix failing test

--- utils/vite.config.ts
+++ utils/vite.config.ts
@@ -1,9 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import dtsPlugin from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [
+		tsconfigPaths({ loose: true }),
 		dtsPlugin({ rollupTypes: true })
 	],
 	build: {
