46aecfbb45fa555f959a01f9ec6412550d7ac333
Candid Dauth <cdauth@cdauth.eu>
2025-11-25 16:50:51 +0100

    Support quoted search terms in route query (#355)

--- types/src/base.ts
+++ types/src/base.ts
@@ -89,3 +89,5 @@ type Builtin = Primitive | Function | Date | Error | RegExp;
 export type DeepReadonly<T> = T extends Builtin ? T : T extends Map<infer K, infer V> ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> : T extends ReadonlyMap<infer K, infer V> ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> : T extends WeakMap<infer K, infer V> ? WeakMap<DeepReadonly<K>, DeepReadonly<V>> : T extends Set<infer U> ? ReadonlySet<DeepReadonly<U>> : T extends ReadonlySet<infer U> ? ReadonlySet<DeepReadonly<U>> : T extends WeakSet<infer U> ? WeakSet<DeepReadonly<U>> : T extends Promise<infer U> ? Promise<DeepReadonly<U>> : T extends {} ? {
     readonly [K in keyof T]: DeepReadonly<T[K]>;
 } : Readonly<T>;
+
+export type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
